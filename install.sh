01827ec439969f0b78471a34abfdea6368d7178a
Ozgur Ozcitak <ozcitak@yahoo.com>
2016-04-09 01:00:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 # Set symlinks to version controlled dot folders
 
 # bash
-ln -sf "$HOME/dotfiles/bash/bashrc" "$HOME/.bashrc"
+ln -sf "$HOME/.dotfiles/bash/bashrc" "$HOME/.bashrc"
 source "$HOME/.bashrc"
 
 # vim
-ln -sf "$HOME/dotfiles/vim" "$HOME/.vim"
-ln -sf "$HOME/dotfiles/vim/vimrc" "$HOME/.vimrc"
+ln -sf "$HOME/.dotfiles/vim" "$HOME/.vim"
+ln -sf "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
 
 # git
-ln -sf "$HOME/dotfiles/git/gitconfig" "$HOME/.gitconfig"
+ln -sf "$HOME/.dotfiles/git/gitconfig" "$HOME/.gitconfig"
 
 # fonts
-ln -sf "$HOME/dotfiles/fonts" "$HOME/.fonts"
+ln -sf "$HOME/.dotfiles/fonts" "$HOME/.fonts"
 
